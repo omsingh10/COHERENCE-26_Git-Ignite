06d2e5603d77ae2fba4417845cadcec95b779fb8
omsingh10 <os0106897@gmail.com>
2026-03-07 00:43:10 +0530

    HUMAN-APP

--- frontend/hisab_kitab/android/app/build.gradle.kts
+++ frontend/hisab_kitab/android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
+    }   
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
